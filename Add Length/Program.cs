d9d14a4d378c1b47980d737230fb73ecb4e8f285
Julia Dubas <uliadubas94@gmail.com>
2022-07-18 21:03:18 +0300

    Add project files.

---
+++ Add Length/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "apple ban";
+
+            string[] newArray = one.Split(' ');
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i] = newArray[i] + ' ' + newArray[i].Length + (newArray.Length - 1 == i ? "" : ","));
+
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
